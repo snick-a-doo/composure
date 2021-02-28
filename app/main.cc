222d9438d1d653fb7ccb0a2122810dd27fbd7e91
Sam Varner <snick-a-doo@comcast.net>
2021-02-27 20:57:01 -0500

    Added the ability to get defaults from a log file
    If a .log file is given as an argument, settings are taken from that file. Command-line
    options override those options.
    
    There was a subtle bug in development. If the key was random when the log file was
    generated, it was being set explicitly from the value in the log file. Since a random
    number wasn't generated, the generator was in a different state when it was used for note
    generation. If an option other than seed is random in the log file it must also be generated
    when the log file is passed as an argument. The seed guarantees it will be the same value
    for the option.

--- app/main.cc
+++ app/main.cc
@@ -16,23 +16,32 @@
 #include <phrase.hh>
 #include <random.hh>
 
+#include <getopt.h>
+
 #include <cassert>
+#include <filesystem>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
 #include <string>
-#include <getopt.h>
 #include <optional>
 #include <random>
+#include <regex>
 
 static std::string version = "1.1.0";
+
+double beat_to_sec(double beats, double tempo)
+{
+    return beats * 60.0 / tempo;
+}
 
 std::string size_and_time(const std::vector<Note>& notes, int tempo)
 {
     // Convert note times from beats to seconds.
     auto sec = notes.empty()
-        ? 0 : static_cast<int>(60.0/tempo * (notes.back().time + notes.back().duration));
+        ? 0
+        : static_cast<int>(beat_to_sec(notes.back().time + notes.back().duration, tempo));
     std::ostringstream os;
     os << std::setw(4) << notes.size() << " notes "
        << std::setw(2) << sec/60 << ':'
@@ -40,8 +49,9 @@ std::string size_and_time(const std::vector<Note>& notes, int tempo)
     return os.str();
 }
 
-/// Make a new composition and write it to a MIDI file.
-int main(int argc, char* argv[])
+/// The command-line options and defaults. Optional entries are set randomly if not
+/// specified.
+struct Options
 {
     std::string output = "composure";
     int voices = 6;
@@ -52,7 +62,69 @@ int main(int argc, char* argv[])
     std::optional<unsigned int> seed;
     bool monophonic = false;
     bool chromatic = false;
+};
 
+/// If a log file was passed as an argument, set the command-line options from the
+/// settings in the file.
+void read_options(int argc, char* argv[], Options& opt)
+{
+    auto set = [&opt](const auto& label, const auto& value, bool random) {
+        // Don't set the option if it was originally chosen randomly. It must be chosen
+        // randomly again (from the same seed) so the generator is in the same state as in
+        // the original when it gets to compose().
+        if (random && label != "seed")
+            return;
+        if (label == "output")
+            opt.output = value;
+        else if (label == "voices")
+            opt.voices = std::stoi(value);
+        else if (label == "passes")
+            opt.passes = std::stoi(value);
+        else if (label == "range")
+            opt.range = std::stoi(value);
+        else if (label == "tempo")
+            opt.tempo = std::stoi(value);
+        else if (label == "seed")
+            opt.seed = std::stoul(value);
+        else if (label == "key")
+            opt.key = std::stoi(value);
+        else if (label == "monophonic")
+            opt.monophonic = value == "yes";
+        else if (label == "chromatic")
+            opt.chromatic = value == "yes";
+        else
+            assert(false); // Unknown label/value pair in log file.
+    };
+
+    for (int i = 1; i < argc; ++i)
+    {
+        std::filesystem::path arg(argv[i]);
+        if (arg.extension() == ".log")
+        {
+            std::ifstream log(arg);
+            std::string line;
+            // Match lines like
+            // key: 60
+            // key (random): 60
+            std::regex label_val_re("^([a-z]+)(.*): ([a-z0-9]+)$");
+            std::smatch match;
+            while (log)
+            {
+                std::getline(log, line);
+                if (std::regex_match(line, match, label_val_re) && match.size() >= 3)
+                    set(match[1].str(), match[3].str(), match[2].length() != 0);
+            }
+        }
+    }
+}
+
+/// Make a new composition and write it to a MIDI file.
+int main(int argc, char* argv[])
+{
+    Options opt;
+    // Set default options from the log file if specified. They may be overridden by
+    // command-line options parsed below.
+    read_options(argc, argv, opt);
     while (true)
     {
         static struct option options[] = {
@@ -77,31 +149,31 @@ int main(int argc, char* argv[])
         case 0:
             break;
         case 'o':
-            output = optarg;
+            opt.output = optarg;
             break;
         case 'v':
-            voices = std::stoi(optarg);
+            opt.voices = std::stoi(optarg);
             break;
         case 'p':
-            passes = std::stoi(optarg);
+            opt.passes = std::stoi(optarg);
             break;
         case 'r':
-            range = std::stoi(optarg);
+            opt.range = std::stoi(optarg);
             break;
         case 't':
-            tempo = std::stoi(optarg);
+            opt.tempo = std::stoi(optarg);
             break;
         case 'k':
-            key = std::stoi(optarg);
+            opt.key = std::stoi(optarg);
             break;
         case 's':
-            seed = std::stoul(optarg);
+            opt.seed = std::stoul(optarg);
             break;
         case 'm':
-            monophonic = true;
+            opt.monophonic = true;
             break;
         case 'c':
-            chromatic = true;
+            opt.chromatic = true;
             break;
         case 'h':
             std::cerr << "Composure: Naive music composition\n"
@@ -114,64 +186,69 @@ int main(int argc, char* argv[])
                       << "    -h --help       Display this help and exit.\n"
                       << "    -k --key=       60 for middle C (random 54 to 65)\n"
                       << "    -m --monophonic (false)\n"
-                      << "    -o --output=    Output file name (" << output << ")\n"
-                      << "    -p --passes=    Number of compose/edit passes (" << passes << ")\n"
-                      << "    -r --range=     Maximum range of notes (" << range << ")\n"
+                      << "    -o --output=    Output file name (" << opt.output << ")\n"
+                      << "    -p --passes=    Number of compose/edit passes (" << opt.passes << ")\n"
+                      << "    -r --range=     Maximum range of notes (" << opt.range << ")\n"
                       << "    -s --seed=      Random seed (random)\n"
-                      << "    -t --tempo=     Beats per minute (" << tempo << ")\n"
-                      << "    -v --voices=    Number of voices (" << voices << ")\n"
+                      << "    -t --tempo=     Beats per minute (" << opt.tempo << ")\n"
+                      << "    -v --voices=    Number of voices (" << opt.voices << ")\n"
                       << std::endl;
             exit(c == 'h' ? 0 : 1);
         }
     }
 
-    std::ofstream log(output + ".log");
+    std::ofstream log(opt.output + ".log");
     for (int i = 0; i < argc; ++i)
         log << argv[i] << ' ';
     log << '\n';
     log << "version: " << version << '\n'
-        << "voices: " << voices << '\n'
-        << "passes: " << passes << '\n'
-        << "range: "  << range << '\n'
-        << "tempo: "  << tempo << '\n';
+        << "output: " << opt.output << '\n'
+        << "voices: " << opt.voices << '\n'
+        << "passes: " << opt.passes << '\n'
+        << "range: "  << opt.range << '\n'
+        << "tempo: "  << opt.tempo << '\n';
 
-    log << "seed " << (seed ? "" : "(random)") << ": ";
+    log << "seed" << (opt.seed ? "" : " (random)") << ": ";
     std::random_device random;
-    if (!seed)
-        seed = random();
-    log << *seed << '\n';
+    if (!opt.seed)
+        opt.seed = random();
+    log << *opt.seed << '\n';
 
-    set_random_seed(*seed);
+    set_random_seed(*opt.seed);
 
     // Pick a random key from MIDI note 54 to 65: F# below middle C to F above.
-    log << "key " << (key ? "" : "(random)") << ": ";
-    if (!key)
-        key = pick(54, 65);
-    log << *key << '\n'
-        << "monophonic: " << (monophonic ? "yes" : "no") << '\n'
-        << "chromatic: " << (chromatic  ? "yes" : "no") << '\n';
+    log << "key" << (opt.key ? "" : " (random)") << ": ";
+    if (!opt.key)
+        opt.key = pick(54, 65);
+    log << *opt.key << '\n'
+        << "monophonic: " << (opt.monophonic ? "yes" : "no") << '\n'
+        << "chromatic: " << (opt.chromatic  ? "yes" : "no") << '\n';
 
     // Start with an empty phrase and iterate.
-    Phrase phrase(tempo);
-    for (int i = 0; i < passes; ++i)
+    Phrase phrase(opt.tempo);
+    for (int i = 0; i < opt.passes; ++i)
     {
-        log << "pass " << i + 1 << '/' << passes << '\n';
-        phrase.compose(*key, voices, range, chromatic);
-        log << "  compose: " << size_and_time(phrase.notes(), tempo) << '\n';
+        log << "pass " << i + 1 << '/' << opt.passes << '\n';
+        phrase.compose(*opt.key, opt.voices, opt.range, opt.chromatic);
+        log << "  compose: " << size_and_time(phrase.notes(), opt.tempo) << '\n';
         phrase.edit();
-        log << "  edit   : " << size_and_time(phrase.notes(), tempo) << '\n';
+        log << "  edit   : " << size_and_time(phrase.notes(), opt.tempo) << '\n';
     }
 
-    std::ofstream file(output + ".midi");
-    phrase.write_midi(file, monophonic);
+    std::ofstream file(opt.output + ".midi");
+    phrase.write_midi(file, opt.monophonic);
 
     // Write a text file with information about each note.
-    std::ofstream note_log(output + ".notes");
+    std::ofstream note_log(opt.output + ".notes");
     for (const auto& note : phrase.notes())
-        note_log << note.time << ' ' << note.time + note.duration << ' '
+    {
+        note_log << beat_to_sec(note.time, opt.tempo) << ' '
+                 << beat_to_sec(note.time + note.duration, opt.tempo) << ' '
                  << note.pitch << ' ' << note.generation << std::endl;
+    }
 
-    // Print out the number of notes and total time.
-    std::cout << size_and_time(phrase.notes(), tempo) << std::endl;
+    // Print out the number of notes, total time, and random seed.
+    std::cout << size_and_time(phrase.notes(), opt.tempo) << "  "
+              << *opt.seed << std::endl;
     return 0;
 }
